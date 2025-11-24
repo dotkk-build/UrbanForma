b6599fd9383928118e21de1c60ed7d20995fbee4
Dotkk <g2153499549@gmail.com>
2025-11-24 14:06:12 +0800

    修复创造模式标签页，添加所有已定义的方块

--- src/main/java/com/urbanforma/ModCreativeTabs.java
+++ src/main/java/com/urbanforma/ModCreativeTabs.java
@@ -8,6 +8,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModCreativeTabs {
     // 都市构方基础物品栏
     public static final CreativeModeTab URBANFORMA_BASE_TAB = new CreativeModeTab("urbanforma_base") {
@@ -18,52 +22,37 @@ public class ModCreativeTabs {
 
         @Override
         public void fillItemList(NonNullList<ItemStack> items) {
-            // 白色系列方块
-            addBlocks(items, ModBlocks.WHITE_BUILDING_BLOCK, ModBlocks.WHITE_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.WHITE_SLAB_BUILDING_BLOCK, ModBlocks.WHITE_STAIRS_BUILDING_BLOCK);
-            
-            // 亮灰系列方块 (1-3)
-            addBlocks(items, ModBlocks.LIGHT_GRAY_1_BUILDING_BLOCK, ModBlocks.LIGHT_GRAY_1_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.LIGHT_GRAY_1_SLAB_BUILDING_BLOCK, ModBlocks.LIGHT_GRAY_1_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.LIGHT_GRAY_2_BUILDING_BLOCK, ModBlocks.LIGHT_GRAY_2_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.LIGHT_GRAY_2_SLAB_BUILDING_BLOCK, ModBlocks.LIGHT_GRAY_2_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.LIGHT_GRAY_3_BUILDING_BLOCK, ModBlocks.LIGHT_GRAY_3_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.LIGHT_GRAY_3_SLAB_BUILDING_BLOCK, ModBlocks.LIGHT_GRAY_3_STAIRS_BUILDING_BLOCK);
-            
-            // 中灰系列方块 (1-3)
-            addBlocks(items, ModBlocks.MEDIUM_GRAY_1_BUILDING_BLOCK, ModBlocks.MEDIUM_GRAY_1_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.MEDIUM_GRAY_1_SLAB_BUILDING_BLOCK, ModBlocks.MEDIUM_GRAY_1_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.MEDIUM_GRAY_2_BUILDING_BLOCK, ModBlocks.MEDIUM_GRAY_2_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.MEDIUM_GRAY_2_SLAB_BUILDING_BLOCK, ModBlocks.MEDIUM_GRAY_2_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.MEDIUM_GRAY_3_BUILDING_BLOCK, ModBlocks.MEDIUM_GRAY_3_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.MEDIUM_GRAY_3_SLAB_BUILDING_BLOCK, ModBlocks.MEDIUM_GRAY_3_STAIRS_BUILDING_BLOCK);
-            
-            // 暗灰系列方块 (1-4)
-            addBlocks(items, ModBlocks.DARK_GRAY_1_BUILDING_BLOCK, ModBlocks.DARK_GRAY_1_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.DARK_GRAY_1_SLAB_BUILDING_BLOCK, ModBlocks.DARK_GRAY_1_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.DARK_GRAY_2_BUILDING_BLOCK, ModBlocks.DARK_GRAY_2_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.DARK_GRAY_2_SLAB_BUILDING_BLOCK, ModBlocks.DARK_GRAY_2_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.DARK_GRAY_3_BUILDING_BLOCK, ModBlocks.DARK_GRAY_3_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.DARK_GRAY_3_SLAB_BUILDING_BLOCK, ModBlocks.DARK_GRAY_3_STAIRS_BUILDING_BLOCK);
-            addBlocks(items, ModBlocks.DARK_GRAY_4_BUILDING_BLOCK, ModBlocks.DARK_GRAY_4_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.DARK_GRAY_4_SLAB_BUILDING_BLOCK, ModBlocks.DARK_GRAY_4_STAIRS_BUILDING_BLOCK);
-            
-            // 黑灰系列方块
-            addBlocks(items, ModBlocks.BLACKISH_GRAY_BUILDING_BLOCK, ModBlocks.BLACKISH_GRAY_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.BLACKISH_GRAY_SLAB_BUILDING_BLOCK, ModBlocks.BLACKISH_GRAY_STAIRS_BUILDING_BLOCK);
-            
-            // 黑色系列方块
-            addBlocks(items, ModBlocks.BLACK_BUILDING_BLOCK, ModBlocks.BLACK_GLOWING_BUILDING_BLOCK, 
-                     ModBlocks.BLACK_SLAB_BUILDING_BLOCK, ModBlocks.BLACK_STAIRS_BUILDING_BLOCK);
-        }
-        
-        private void addBlocks(NonNullList<ItemStack> items, 
-                              RegistryObject<Block> block, RegistryObject<Block> glowing, 
-                              RegistryObject<Block> slab, RegistryObject<Block> stairs) {
-            items.add(new ItemStack(block.get()));
-            items.add(new ItemStack(glowing.get()));
-            items.add(new ItemStack(slab.get()));
-            items.add(new ItemStack(stairs.get()));
+            // 使用反射获取所有方块
+            try {
+                List<RegistryObject<Block>> basicBlocks = new ArrayList<>();
+                List<RegistryObject<Block>> roadBlocks = new ArrayList<>();
+                
+                Field[] fields = ModBlocks.class.getDeclaredFields();
+                for (Field field : fields) {
+                    if (field.getType().equals(RegistryObject.class) && 
+                        field.getGenericType().toString().contains("Block")) {
+                        
+                        field.setAccessible(true);
+                        RegistryObject<Block> block = (RegistryObject<Block>) field.get(null);
+                        
+                        // 将道路方块和建筑方块分开
+                        String name = field.getName();
+                        if (name.startsWith("ROAD_")) {
+                            roadBlocks.add(block);
+                        } else {
+                            basicBlocks.add(block);
+                        }
+                    }
+                }
+                
+                // 添加所有基础建筑方块
+                for (RegistryObject<Block> block : basicBlocks) {
+                    items.add(new ItemStack(block.get()));
+                }
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     };
     
@@ -76,24 +65,25 @@ public class ModCreativeTabs {
 
         @Override
         public void fillItemList(NonNullList<ItemStack> items) {
-            // 道路系列方块
-            items.add(new ItemStack(ModBlocks.ROAD_A_GRAY.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_GRAY_GLOWING.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_GRAY_SLAB.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_LIGHT_GRAY.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_LIGHT_GRAY_GLOWING.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_LIGHT_GRAY_SLAB.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_GLOWING.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_SLAB.get()));
-            
-            // 道路标线
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_LINE.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_45_DEGREE.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_ZEBRA.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_CROSS.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_T_LINE.get()));
-            items.add(new ItemStack(ModBlocks.ROAD_A_WHITE_CORNER.get()));
+            // 使用反射获取所有道路方块
+            try {
+                Field[] fields = ModBlocks.class.getDeclaredFields();
+                for (Field field : fields) {
+                    if (field.getType().equals(RegistryObject.class) && 
+                        field.getGenericType().toString().contains("Block")) {
+                        
+                        String name = field.getName();
+                        if (name.startsWith("ROAD_")) {
+                            field.setAccessible(true);
+                            RegistryObject<Block> block = (RegistryObject<Block>) field.get(null);
+                            items.add(new ItemStack(block.get()));
+                        }
+                    }
+                }
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     };
     
